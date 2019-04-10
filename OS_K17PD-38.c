63265abf541d59ecac75efe6c4ec35668f9b33cd
Rahul <31037131+Rahul19737@users.noreply.github.com>
2019-04-10 23:44:00 +0530

    Add files via upload

---
+++ OS_K17PD-38.c
@@ -0,0 +1,40 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int n;    //number of process
+	int r;      //number of resources
+	int i,j,k,cnt,cntt;
+	int avail[10],p[10];
+	int need[10][10],alloc[10][10],max[10][10];
+
+	printf("\nEnter number of process :");
+	scanf("%d",&n);
+	printf("\n Enter  resources available : ");
+	scanf("%d",&r);
+	printf("\nEnter insatnces for resources :\n");
+	for(i=0;i<r;i++)
+	{  
+		printf("R%d ",i+1);
+		scanf("%d",&avail[i]);
+	}
+	printf("\n Enter allocation matrix  \n");
+	for(i=0;i<n;i++)
+	{
+		printf("p%d",i+1);          
+		p[i]=0;
+		for(j=0;j<r;j++)
+ 		{
+			scanf("%d",&alloc[i][j]);
+ 		}
+	}
+  	printf("\n Enter MAX matrix  \n");
+
+ 	for(i=0;i<n;i++)
+	{
+		printf("p%d",i+1);
+		for(j=0;j<r;j++)
+ 		{
+  			scanf("%d",&max[i][j]);
+ 		}
+ 	}
